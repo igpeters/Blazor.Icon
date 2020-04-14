3b16e8f97a3ebbf3b0af7ba5e1714262879a4717
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2020-04-14 06:12:55 -0400

    Moved from src to nuget

--- src/Blazor/Icon.razor.cs
+++ src/Blazor/Icon.razor.cs
@@ -29,5 +29,31 @@ namespace Mobsites.Blazor
         /// Margin spacing between icon and accompanying text. Ignored when text is empty.
         /// </summary>
         [Parameter] public int Spacing { get; set; }
+
+        /// <summary>
+        /// Whether the icon should show an inactive state.
+        /// </summary>
+        [Parameter] public bool Inactive { get; set; }
+
+        /// <summary>
+        /// Any valid css color usage.
+        /// </summary>
+        [Parameter] public string InactiveColor { get; set; }
+
+        private string @class;
+
+        /// <summary>
+        /// Styles for directly affecting this component go here.
+        /// </summary>
+        [Parameter] public override string Class
+        { 
+            get 
+            {
+                string size = this.Size > 0 ? $"md-{(int)this.Size}" : null;
+
+                return $"{size} {@class}";
+            }
+            set => @class = value; 
+        }
     }
 }
